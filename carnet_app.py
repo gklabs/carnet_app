461bc82e539e28510a2c5ecb8b693c43a0ad89dc
gklabs <giridharkaushikr@gmail.com>
2021-02-18 01:16:54 -0500

    streamlit py

--- carnet_app.py
+++ carnet_app.py
@@ -4,5 +4,8 @@ import pandas as pd
 st.title(" CAR-NET: Carbon Neutral Evaluation Tool")
 st.write(" Created for Baltimore County by George Mason University")
 
-Base_Budget = st.sidebar.number_input("Budget for 2021:")
-st.write('Budget entered is', Base_Budget)
+Base_Budget = st.sidebar.number_input("Enter Base Procurement Budget for 2021:")
+st.write('Budget for 2021 entered is', Base_Budget)
+
+
+
