befd9b5c0c8df8aa13f74868061bd8bd5f057ffc
gklabs <giridharkaushikr@gmail.com>
2021-02-18 17:00:04 -0500

    added pandas view

--- carnet_app.py
+++ carnet_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.title(" CAR-NET: Carbon Neutral Evaluation Tool")
 st.write(" Created for Baltimore County by George Mason University")
@@ -7,5 +8,6 @@ st.write(" Created for Baltimore County by George Mason University")
 Base_Budget = st.sidebar.number_input("Enter Base Procurement Budget for 2021:")
 st.write('Budget for 2021 entered is', Base_Budget)
 
-
+df = pd.DataFrame(np.random.randn(50, 20),columns=('var %d' % i for i in range(20)))
+st.dataframe(df)  # Same as st.write(df)
 
